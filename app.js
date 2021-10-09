54d52aad80b4ebba9af5b74f736cd4e8f75d9ce4
Iana <malovayai@gmail.com>
2021-10-09 12:31:55 +0300

    Добавление 4 колонки для предела прочности

--- app.js
+++ app.js
@@ -5,32 +5,32 @@ function saveClick() {
     let x = new Array(rowCount);
     let count = rowCount;
     for (let i = 0; i < rowCount; i++) {
-        x[i] = new Array(3);
-        count+=2;
+        x[i] = new Array(4);
+        count+=3;
     }
     //console.log(count)
     //let table = document.getElementsByTagName('mytbl')[0];
-    var tableObj = document.getElementById( "mytbl" );
-    var arr = [];
-    var allTRs = tableObj.getElementsByTagName( "tr" );
-    for ( var trCounter = 0; trCounter < allTRs.length; trCounter++ )
-    {
-        var tmpArr = [];
-        var allTDsInTR = allTRs[ trCounter ].getElementsByTagName( "td" );
-        for ( var tdCounter = 0; tdCounter < allTDsInTR.length; tdCounter++ )
-        {
-            tmpArr.push( allTDsInTR[ tdCounter ].textContent );
-        }
-        arr.push( tmpArr );
-    }
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < 3; j++) {
-            //console.log(arr[i]);
-        }
-    }
-    for(let i = 0; i < count; i++) {
-        //console.log(document.getElementsByTagName("input")[i].value)
-    }
+    // var tableObj = document.getElementById( "mytbl" );
+    // var arr = [];
+    // var allTRs = tableObj.getElementsByTagName( "tr" );
+    // for ( var trCounter = 0; trCounter < allTRs.length; trCounter++ )
+    // {
+    //     var tmpArr = [];
+    //     var allTDsInTR = allTRs[ trCounter ].getElementsByTagName( "td" );
+    //     for ( var tdCounter = 0; tdCounter < allTDsInTR.length; tdCounter++ )
+    //     {
+    //         tmpArr.push( allTDsInTR[ tdCounter ].textContent );
+    //     }
+    //     arr.push( tmpArr );
+    // }
+    // for(let i = 0; i < arr.length; i++) {
+    //     for(let j = 0; j < 3; j++) {
+    //         //console.log(arr[i]);
+    //     }
+    // }
+    // for(let i = 0; i < count; i++) {
+    //     //console.log(document.getElementsByTagName("input")[i].value)
+    // }
     let bigArr = new Array()
     for(let i = 0; i < count; i++) {
         bigArr[i] = document.getElementsByTagName("input")[i].value;document.getElementsByTagName("input")[i].value
@@ -39,17 +39,17 @@ function saveClick() {
 }
 
 function start() {
-    let arr = addRow('mytbl');
-    document.getElementById(`mytbl`).addEventListener(`input`, e => {
-        let tempValue
-        if (e.target.tagName === `INPUT`) {
-            tempValue = e.target.value;
-        } //console.log(e.target.value);
-        console.log(tempValue);
-    });
-
-    for(let i = 0; i < 3; i++) {
-        console.log(arr[i])
-    }
+    // let arr = addRow('mytbl');
+    // document.getElementById(`mytbl`).addEventListener(`input`, e => {
+    //     let tempValue
+    //     if (e.target.tagName === `INPUT`) {
+    //         tempValue = e.target.value;
+    //     } //console.log(e.target.value);
+    //     console.log(tempValue);
+    // });
+    //
+    // for(let i = 0; i < 3; i++) {
+    //     console.log(arr[i])
+    // }
 }
 
