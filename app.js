b8a636c43ec4a78181220255b847c8c9b33b9570
ianamalova <malovayai@gmail.com>
2021-10-28 02:00:16 +0300

    ...

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ function AddRow() {
     $tbl.insertAdjacentHTML("beforeend", "<tr></tr>");
     var $access = document.getElementsByTagName("tr")[counter2];
     $access.insertAdjacentHTML("beforeend", "<td><text></text></td>")
-    $access.insertAdjacentHTML("beforeend", "<td><input value=1 min='0' step=any></td>")
-    $access.insertAdjacentHTML("beforeend", "<td><input value=1 min='0' step=any></td>")
-    $access.insertAdjacentHTML("beforeend", "<td><input value=1 min='0' step=any></td>")
-    $access.insertAdjacentHTML("beforeend", "<td><input value=1 min='0' step=any></td>")
+    $access.insertAdjacentHTML("beforeend", "<td><input type='number' value=1 min='0' step=any></td>")
+    $access.insertAdjacentHTML("beforeend", "<td><input type='number' value=1 min='0' step=any></td>")
+    $access.insertAdjacentHTML("beforeend", "<td><input type='number' value=1 min='0' step=any></td>")
+    $access.insertAdjacentHTML("beforeend", "<td><input type='number' value=1 min='0' step=any></td>")
     counter2++;
     function onChange(evt) {
         //document.querySelector(".result").innerHTML = evt.target.value;
@@ -55,8 +55,6 @@ function AddRow() {
     }
 
 }
-
-
 
 // function addRow1(id){
 //     let tbody = document.getElementById(id).getElementsByTagName("TBODY")[0];
@@ -149,100 +147,7 @@ function AddRow() {
 //     }
 //     //saveElement();
 // }
-// function isNumber(num) {
-//     return typeof num === 'number' && !Number.isNaN(num);
-// }
 
-// function saveClick() {
-//     let rowCount = document.getElementById('mytbl').rows.length - 1; //получение количества строк
-//     //получение кол-ва ячеек(дырок куда вводятся цифры без учета статических данных)
-//     let count = rowCount;
-//     for (let i = 0; i < rowCount; i++) {
-//         count += 3;
-//     }
-//
-//     for (let i = 0; i < count; i++) {
-//         bigArr[i] = document.getElementsByTagName("input")[i].value;
-//     }
-//
-//
-//     let qw = 0;
-//     for (let i = 0; i < count + rowCount; i++) {
-//         arrQ[qw] = 0;
-//     }
-//     //parse the Q array from strings to numbers
-//     let j = 0; //счетчик для массива
-//     for (let i = count; i < count + rowCount; i++) {
-//         arrQ[j] = document.getElementsByTagName("input")[i].value;
-//         j++;
-//     }
-//
-//     for (let i = 0; i < rowCount; i++) {
-//         let temp = parseInt(arrQ[i]);
-//         if (Number.isNaN(temp)) {
-//             alert("Владислав Александрович, это безобразие! :)")
-//             document.write('<img src="https://pbs.twimg.com/profile_images/1021724681752256513/WM5yxh86_400x400.jpg">');
-//             break
-//         } else {
-//             arrQ[i] = temp;
-//         }
-//         console.log(arrQ[i])
-//     }
-//     let counter = count + rowCount;
-//     //parse the F array from strings to numbers
-//     let qw1 = 0;
-//     for (let i = 0; i < count + rowCount; i++) {
-//         arrF[qw1] = 0;
-//     }
-//     let k = 0; //счетчик для второго массива
-//     for (let i = counter; i < counter + rowCount + 1; i++) {
-//         arrF[k] = document.getElementsByTagName("input")[i].value;
-//         k++;
-//     }
-//     for (let i = 0; i < arrF.length; i++) {
-//         let temp = parseInt(arrF[i])
-//         if (Number.isNaN(temp)) {
-//             alert("Владислав Александрович, это безобразие! :)")
-//             document.write('<img src="https://pbs.twimg.com/profile_images/1021724681752256513/WM5yxh86_400x400.jpg">');
-//             break
-//         } else {
-//             arrF[i] = temp;
-//         }
-//         console.log(arrF[i])
-//     }
-//     if (bigArr[0] === 75 & bigArr[1] === 82 & bigArr[2] === 89 & bigArr[3] === 65) {
-//         document.write('<img width="800" height="1000" src="https://sun9-21.userapi.com/impg/B0Vf8RJJvU1-9iI8oMcvzXs72l346GFCLi18hg/iqXFANivWV4.jpg?size=1620x2160&quality=96&sign=ec6e6ef7627812ae5a623ba85cf5da0d&type=album">');
-//     }
-// }
-
-
-// let form = document.querySelector("form");
-// let log = document.querySelector("#log");
-//
-// let flagRake = 0; //переменная отвечающая за заделки
-// form.addEventListener("submit", function (event) {
-//     let data = new FormData(form);
-//     let output = "";
-//     for (const entry of data) {
-//         output = entry[0] + " = " + entry[1] + "\r";
-//         if (entry[1] === 'left rake') {
-//             console.log("Выбрана 1")
-//             flagRake = 1;
-//             console.log(flagRake)
-//         } else if (entry[1] === 'right rake') {
-//             flagRake = 2;
-//             console.log(flagRake)
-//             console.log("Выбрана 2")
-//         } else if (entry[1] === 'both rake') {
-//             flagRake = 3;
-//             console.log(flagRake)
-//             console.log("Выбрана 3")
-//         }
-//     }
-//     ;
-//     //log.innerText = output;
-//     event.preventDefault();
-// }, false);
 
 function inputForces() {
     let rowCount = document.getElementById('mytbl').rows.length - 1;
@@ -651,9 +556,9 @@ function f1() {
     var data2=JSON.parse($.cookie("arrQ"))
     var data3=JSON.parse($.cookie("arrF"))
     //alert(document.cookie)
-    // alert(data1)
-    // alert(data2)
-    // alert(data3)
+    alert(data1)
+    alert(data2)
+    alert(data3)
 }
 
 function printMemasik () {
