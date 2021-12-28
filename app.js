b4bee16a5f43cf67cd77db21d0664a834b565b2a
ianamalova <malovayai@gmail.com>
2021-12-28 22:26:18 +0300

    Исправлена проблема с underfined после считывания пустых куки

--- app.js
+++ app.js
@@ -525,12 +525,15 @@ function Getter() {
     const $tb2 = document.getElementById('tb3');
     let countformain = 0;
     for(let i = 0; i < document.getElementById('maintabl').rows.length*4; i++) {
-        let size = data1.length;
-        document.getElementsByTagName('input')[countformain].value = data1[i];
-        countformain++;
-        if(i+1 === size) {
-            break;
+        if(data1.length !== 0) {
+            let size = data1.length;
+            document.getElementsByTagName('input')[countformain].value = data1[i];
+            countformain++;
+            if(i+1 === size) {
+                break;
+            }
         }
+
     }
     //alert($maintb.rows.length);
     let countfortb2 = document.getElementById('mytbl').rows.length - 1;
